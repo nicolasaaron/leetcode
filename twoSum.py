3ae465650e1a859c124359a321a92a0e19af91cc
nicolasaaron <aaron.9086@gmail.com>
2018-08-21 15:21:17 +0800

    twoSum

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,22 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+
+"""
+first solution
+""" 
+
+
 #import numpy
 
 class Solution:
@@ -20,3 +39,25 @@ class Solution:
         
 #if __name__ == '__main__':
 #    print(Solution().twoSum((2, 7, 11, 15), 9))
+
+
+
+"""
+one hash table
+"""
+
+class Solution:
+	def twoSum(self, nums, target):
+		dic = {}
+		for i, value in enumerate(nums):
+			key = target - value
+			if key in dic:
+				return [dic[key], i]
+			else:
+				dic[value] = i
+
+
+
+
+			
+				
